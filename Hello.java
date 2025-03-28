616f37c3cddc2109b200ea14b251818b36936155
shreems321 <shree.ms321@gmail.com>
2025-03-27 17:11:12 -0700

    my new repo

---
+++ Hello.java
@@ -0,0 +1,43 @@
+class Calculator
+{
+  int a;
+
+  public int add(int n1,int n2)
+  {
+    int r = n1+n2;
+    return r;
+   
+  }
+  public void playMusic()
+  {
+    System.out.println("Playmusic");
+  }
+  public String getMeAPen(int cost)
+  {
+    if (cost<10)
+    return "pen";
+    else 
+    return "pen not found";
+  }
+}
+
+public class Hello
+{
+  public static void main(String args[])
+  {
+    int num1 = 4 ;
+    int num2 = 5;
+    int result = num1+num2;
+    System.out.println(result);
+
+    Calculator obj = new Calculator();
+    Calculator obj1 = new Calculator();
+    //Claculator obj2 = new Calculator();
+    //int result = obj.add(num1,num2);
+    String str = obj1.getMeAPen(12);
+    obj1.playMusic();
+   System.out.println(str);
+    //System.out.println(result);
+
+  }
+}
