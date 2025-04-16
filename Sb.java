cac5f98f24855a62cf8f815cfb46ba33e7420199
shreems321 <shree.ms321@gmail.com>
2025-04-16 15:29:02 -0700

    String buffer

---
+++ Sb.java
@@ -0,0 +1,11 @@
+public class Sb {
+    public static void main(String[]a)
+    {
+        StringBuffer sb = new StringBuffer("Srinivas");
+        sb.append("Sonu");
+       String str = sb.toString();
+
+        System.out.println(sb);
+    } 
+    
+}
