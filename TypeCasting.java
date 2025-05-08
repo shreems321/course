09acb5db176d60fbf9f8137552a3b5dfc623d2f9
shreems321 <shree.ms321@gmail.com>
2025-05-07 17:56:29 -0700

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,30 @@
+
+class A
+{
+public void show1()
+{
+   System.out.println("in A show");
+}
+}
+class B extends A
+{
+ public void show2()
+ {
+   System.out.println("in B show");
+ }
+}  
+
+
+public class TypeCasting
+
+{
+ public static void main(String a[])
+ 
+ {
+   A obj =(A) new B();
+   obj.show1();
+    B obj1 = (B)obj;
+    obj1.show2();
+ }
+
+}
