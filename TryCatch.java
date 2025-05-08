c46f266ce84ccc6f30c81ffbbacbcd031889fa76
shreems321 <shree.ms321@gmail.com>
2025-05-07 21:17:08 -0700

    Example For TryCatch Exception

---
+++ TryCatch.java
@@ -0,0 +1,19 @@
+public class TryCatch
+{
+  
+  public static void main(String a[])
+  {
+   int i = 0;
+   int j = 0;
+  
+   try {
+    j = 20/i;
+   } catch (Exception e) {
+    // TODO: handle exception
+   }
+   {
+    System.out.println(j);
+    System.out.println("bye");
+   }
+  }
+}            
