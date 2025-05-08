9098ce514456d69b56991a0a4ea8de5b2b24dd16
shreems321 <shree.ms321@gmail.com>
2025-05-07 17:26:29 -0700

    Naming Convention

---
+++ Naming.java
@@ -0,0 +1,24 @@
+class a
+{
+    public a()
+    {
+        System.out.println("object created");
+    }
+
+    public void show()
+    {
+        System.out.println("in a show");
+    }
+}
+    
+public class Naming
+{
+    public static void main(String a[])
+    {
+        new a().show();
+        //obj.show();
+
+    }
+   
+}
+    
