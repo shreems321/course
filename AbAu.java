1a1fe296f26924a004e34acb34c19a2b8171a783
shreems321 <shree.ms321@gmail.com>
2025-05-07 18:03:15 -0700

    Auto Boxing and Auto Unboxing

---
+++ AbAu.java
@@ -0,0 +1,20 @@
+
+public class AbAu
+{
+    public static void main(String a[])
+    
+    {
+      int num = 10;
+      Integer num1 = num;   //SAuto BOXING
+
+      int num2 = num1;   // AUTO UNBOXING
+
+      String str = "15";
+      int num3 = Integer.parseInt(str);
+
+      System.out.println(num3*10);
+
+
+    }
+   
+}
