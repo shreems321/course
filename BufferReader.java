d796c870cf84814d0a1be03ddb8c691b60d6f3d4
shreems321 <shree.ms321@gmail.com>
2025-05-07 21:36:53 -0700

    BufferReader Example

---
+++ BufferReader.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.SocketPermission;
+
+public class BufferReader
+{
+  
+  public static void main(String a[]) throws IOException
+  {
+    System.out.println("enter the number");
+   int num = System.in.read();
+   System.out.println(num-48);
+  }
+}   
+
+
+---------------------------------------------------------------------------------------
+/* 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.SocketPermission;
+
+public class Hello
+{
+  
+  public static void main(String a[]) throws IOException
+  {
+    System.out.println("enter the number");
+    InputStreamReader in = new InputStreamReader(System.in);
+   BufferedReader bf = new BufferedReader(in);
+   
+   int num = Integer.parseInt(bf.readLine());
+   System.out.println(num);
+   bf.close();
+  }
+}   
+*/
