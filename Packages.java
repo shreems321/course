3245ffde9e30752e9d9c43139cc52050b94af682
shreems321 <shree.ms321@gmail.com>
2025-05-07 17:44:09 -0700

    Packages

---
+++ Packages.java
@@ -0,0 +1,18 @@
+//import others.tools.AdvCalc;
+import others.tools.*;
+
+
+public class Hello
+{
+    public static void main(String a[])
+    {
+    // AdvCalc obj = new AdvCalc();
+      //Calc obj = new Calc();
+      int r1 = obj.add(2,5);
+     
+    
+      System.out.println(r1 );
+
+    }
+   
+}  
