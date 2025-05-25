7a8aa81a89e3adec823fa8807a5234fe22d24982
shreems321 <shree.ms321@gmail.com>
2025-05-24 22:47:32 -0700

    Find max Consicutive Number of 1s

---
+++ ConsicutiveOnes.java
@@ -0,0 +1,17 @@
+public class ConsicutiveOnes {
+    public int findMaxConsicutiveOnes(int[]nums){
+        int ans = 0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]=0);
+            count = 0;
+        }
+        else{
+            cout++;
+        }
+        if(count>ans)
+        ans = count;
+
+    }
+    return ans;
+}
